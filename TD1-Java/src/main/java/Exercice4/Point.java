8cafaac30b7c0d50d8d672c97e4f531b8199840b
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2019-09-21 22:50:33 +0200

    Exercice 5 finished

--- TD1-Java/src/main/java/Exercice4/Point.java
+++ TD1-Java/src/main/java/Exercice4/Point.java
@@ -1,5 +1,7 @@
 package Exercice4;
 
+import java.util.ArrayList;
+
 public class Point {
 	private int x;
 	private int y;
@@ -22,22 +24,32 @@ public class Point {
 	}
 
 	public static void main(String[] args) {
-		Point p = new Point(2,1);
-		String point = "("+p.x+","+p.y+")";
-		System.out.println(compteurDePointsCréés);
-		System.out.println(point);
-		Point o = new Point();
-		point = "("+o.x+","+o.y+")";
-		System.out.println(compteurDePointsCréés);
-		System.out.println(point);
-		Point i = new Point(2);
-		point = "("+i.x+","+i.y+")";
-		System.out.println(point);
-		System.out.println(compteurDePointsCréés);
-		//Réponse question 4.1 
-		//=> Cela fonction car les données x et y appartiennent 
-		//à la classe et sont initialement initée à zéro lors de la création de l'objet.
-
+		Point p1 = new Point(1,2);
+		Point p2 = p1;
+		Point p3 = new Point(1,2);
+		//Réponse Q 5.1 : 
+		//System.out.println(p1==p2);
+		//System.out.println(p2==p3);
+		//Le code affiche true false car les objets sont comparés
+		//et pas leurs attributs
+		ArrayList<Point> list= new ArrayList<Point>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		System.out.println(list.indexOf(p2));
+		System.out.println(list.indexOf(p1));
+		System.out.println(list.indexOf(p3));
+		//Utilise get(int index)
+		System.out.println(p1.equals(new String()));
+		//False car p1 ne pas être égale à un objet sans lien d'occurence
+		
 	}
+	public boolean isSamePoint(Point p1,Point p2) {
+		if(p1.x == p2.x && p1.y ==p2.y) {
+			return true;
+		}
+		return false;
+	}
+	
 
 }
---
+++ TD1-Java/src/main/java/Exercice5/Point.java
@@ -0,0 +1,43 @@
+package Exercice5;
+
+public class Point {
+	private int x;
+	private int y;
+	private static int compteurDePointsCréés = 0;
+	
+	public Point(int x,int y) {
+		this.x = x;
+		this.y = y;
+		compteurDePointsCréés++;
+		
+	}
+	
+	public Point(int x) {
+		this.x = x;
+		compteurDePointsCréés++;
+		
+	}
+	public Point() {
+		compteurDePointsCréés++;
+	}
+
+	public static void main(String[] args) {
+		Point p = new Point(2,1);
+		String point = "("+p.x+","+p.y+")";
+		System.out.println(compteurDePointsCréés);
+		System.out.println(point);
+		Point o = new Point();
+		point = "("+o.x+","+o.y+")";
+		System.out.println(compteurDePointsCréés);
+		System.out.println(point);
+		Point i = new Point(2);
+		point = "("+i.x+","+i.y+")";
+		System.out.println(point);
+		System.out.println(compteurDePointsCréés);
+		//Réponse question 4.1 
+		//=> Cela fonction car les données x et y appartiennent 
+		//à la classe et sont initialement initée à zéro lors de la création de l'objet.
+
+	}
+
+}
